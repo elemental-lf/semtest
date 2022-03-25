74d9700562b718cb3f7cb8046a1e38fb93fad5e1
Lars Fenneberg <lf@elemental.net>
2022-03-25 15:28:42 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.0.0"
+
+setup(name='semtest',
+      version=__version__,
+      description='Sem Test',
+      author='Lars Fenneberg',
+      author_email='lf@elemental.net',
+      license='LGPG-3',
+      python_requires='~=3.6',
+      packages=find_packages('src'),
+      package_dir={
+          '': 'src',
+      },
+)
